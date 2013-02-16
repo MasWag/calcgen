4c7dabf5e196f53a423cc00ded6730609dd9882b
Masaki Waga <tsugarutamenobu@gmail.com>
2013-02-16 18:51:53 +0900

    printgen modified

--- printgen.sh
+++ printgen.sh
@@ -6,6 +6,25 @@
 ############################
 
 
+if [ $# != 1 ] && [ $# != 2 ]; then 
+    echo "usage:$0  sort num";
+    exit;
+fi
+
+if [ $1 == "-h" ] || [ $1 == "-help" ]; then
+    cat << EOS
+計算問題自動生成スクリプト
+========================
+使い方:$0  種類 個数
+
+種類
+------------------------------
+問題の種類の番号と内容の対応は以下の様になっています。
+1:正負の数の四則演算
+EOS
+    exit;
+fi
+
 cat <<EOS
 \documentclass{jsarticle}
 \setlength{\topmargin}{-25mm}
@@ -16,8 +35,8 @@ cat <<EOS
 EOS
 
 case $1 in
-1)
-echo "正負の数計算問題";;
+    1)
+	echo "\section*{正負の数計算問題}";;
 esac
 
 cat << EOS
@@ -26,8 +45,8 @@ cat << EOS
 EOS
 
 case $1 in
-1)
-./calcgen1.sh $2;;
+    1)
+	./calcgen1.sh $2;;
 esac
 
 cat << EOS
